7bd3ce8bd8d6e3cf778fdb054db1212c9026a476
JorgeGutierrezP <105743070+JorgeGutierrezP@users.noreply.github.com>
2022-05-31 11:26:50 -0700

    Version 1

---
+++ Escritura archivos/Escritura archivos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Escritura_archivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            //si el archivo no existe lo creará
+            //y si existe escribirá en el
+
+            string[] lines =
+            {
+                "Esta es la nueva escritura",
+                "Esta es la segunda linea",
+                "fin del texto"
+            };  
+            foreach(string line in lines)
+            {
+                sw.WriteLine(lines);
+            }
+            sw.Close(); //siempre se debe cerrar el archivo
+            Console.WriteLine("Escribiendo en el archivo...");
+            Console.ReadLine();
+        }
+    }
+}
