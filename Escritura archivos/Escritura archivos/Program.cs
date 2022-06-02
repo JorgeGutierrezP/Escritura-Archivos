3261f1c315b464e07e6f54720efc6b96658158b9
JorgeGutierrezP <105743070+JorgeGutierrezP@users.noreply.github.com>
2022-06-02 10:30:05 -0700

    Version 3

--- Escritura archivos/Escritura archivos/Program.cs
+++ Escritura archivos/Escritura archivos/Program.cs
@@ -10,28 +10,51 @@ namespace Escritura_archivos
 {
     internal class Program
     {
+        class Registro_Empleo
+        {
+            public string nombre;
+            public string apellidos;
+            public string domilicio;
+            public string RFC;
+
+            //constructor
+            public Registro_Empleo(string nombre, string apellidos, string domicilio, string RFC)
+            {
+                this.nombre = nombre;
+                this.apellidos = apellidos;
+                this.domilicio = domicilio;
+                this.RFC = RFC;
+            }
+        }
         static void Main(string[] args)
         {
-            string Nom;
-            StreamWriter sw = new StreamWriter("Ejemplo.txt", true);
-            //si el archivo no existe lo creara
-            //si ya existe, escribira en el
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            //si el archivo no existe lo creará
+            //si ya existe, escribirá en él
+            //true es para agregar y no para sobreescribir
 
-            Console.Write("Ingresa un nombre: ");
-            Nom = Convert.ToString(Console.ReadLine());
+            string nombre, apellidos, domicilio, RFC;
 
-            string[] Lines ={
-                Nom
 
-            };
+            Console.Write("Ingrese un nombre: ");
+            nombre = Console.ReadLine();
 
-            foreach (string Line in Lines)
-            {
-                sw.WriteLine(Line);
-            }
-            sw.Close();//se debe cerrare el archivo
+            Console.Write("Ingrese sus apellidos: ");
+            apellidos = Console.ReadLine();
 
-            Console.WriteLine("Escrbiendo en el archivo.....");
+            Console.Write("Ingrese su domicilio: ");
+            domicilio = Console.ReadLine();
+
+            Console.Write("Ingresa su RFC: ");
+            RFC = Console.ReadLine();
+
+            Registro_Empleo Persona = new Registro_Empleo(nombre, apellidos, domicilio, RFC);
+
+            sw.WriteLine(Persona.nombre + "  " + Persona.apellidos + "  " + Persona.domilicio + "  " + Persona.RFC);
+
+            sw.Close(); //Siempre se debe cerrar el archivo
+
+            Console.WriteLine("Escribiendo en el archivo.........");
             Console.ReadLine();
         }
     }
